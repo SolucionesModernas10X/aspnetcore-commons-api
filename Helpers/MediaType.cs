d8e7266190d2cafd22dfa851af85f5f2a73727b2
10x Dev Team <105618681+Dev10x@users.noreply.github.com>
2022-05-17 18:22:35 -0600

    added basic classes
    -added commons api exception
    -added commons json date converter

---
+++ Helpers/MediaType.cs
@@ -0,0 +1,38 @@
+﻿//
+//  Copyright 2022  Soluciones Modernas 10x
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+namespace Dev10x.AspnetCore.Commons.Api.Helpers
+{
+    public class MediaType
+    {
+        public MediaType()
+        {
+        }
+
+
+		///Media type for PDF.
+		public static readonly string APP_PDF = "application/pdf";
+
+		///Media type for PDF.
+		public static readonly string PDF = "pdf";
+
+		//extension for XLS
+		public static readonly string XLS = "xlsx";
+
+		///Media type for XLS
+		public static readonly string APP_XLS = "application/vnd.ms-excel";
+
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # aspnetcore-commons-api
  aspnetcore-commons-api
+
+ # framework
+ Asp.Net Core 5
+
+ # dependences
+ Microsoft.AspNetCore.MvcCore 2.2.5+
